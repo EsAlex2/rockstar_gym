dbbf8769ccfd4dc466a7ef195bda9f51b3bc98f5
Alex Madrid <alexmadrid326@gmail.com>
2026-06-03 17:52:36 -0400

    nuevos cambios

---
+++ app/models/clientesModels.php
@@ -0,0 +1,42 @@
+<?php 
+
+require_once __DIR__ . '/models.php';
+require_once __DIR__ . '/../core/conn.php';
+
+class clientesModel extends Model
+{
+    protected $pdo;
+    protected int $estatus_id; 
+    protected int $persona_id;
+    protected int $rol_id;
+    protected string $username;
+    protected string $email;
+    protected string $password;
+    protected array $mensajes = [];
+
+    public function __construct($pdo)
+    {   
+        parent::__construct($pdo);
+        $this->pdo = $pdo;
+    }
+
+    public function obtenerClientes()
+    {
+        $this->mensajes = [
+            "Error de conexion a la base de datos",
+            "Error inesperado al obtener los clientes"
+        ];
+
+        try{
+
+            if(!$this->pdo){
+                return $this->mensajes[0];
+            }
+
+            
+
+        } catch (PDOException $e) {
+            return $this->mensajes[1] . ": " . $e->getMessage();
+        }
+    }
+}
